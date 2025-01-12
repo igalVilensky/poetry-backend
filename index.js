9f7e931a42548a2d30715f840f6797edeeebb655
Igal <igal.vilensky@flowplace.com>
2025-01-12 11:11:21 +0100

    Initial commit for backend

---
+++ index.js
@@ -0,0 +1,12 @@
+// poetry-backend/index.js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
